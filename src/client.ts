a5f8ab9893c5097be9f55721260950bf7c3f9140
Tchoupinax <corentinfiloche@hotmail.fr>
2021-11-27 08:48:14 +0100

    feat: allow header for each request

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,5 @@
 
 import axios, { AxiosPromise } from 'axios'
-import { SubscriptionClient } from 'graphql-subscriptions-client';
 import { print } from 'graphql/language/printer';
 
 export default class GraphqlClient {
@@ -14,21 +13,36 @@ export default class GraphqlClient {
     }
 
     this.url = url;
-    this.headers = headers;
+    this.headers = headers ?? {};
 
     // NOTE: automatically replace http by ws and https by wss for the subscription query
     let wsUrl = url.replace(/http:\/\//, 'ws://').replace(/https:\/\//, 'wss://')
 
-    this.subscriptionClient = new SubscriptionClient(wsUrl, {
-      reconnect: true,
-      lazy: true, // only connect when there is a query
-      connectionCallback: error => {
-        error && console.error(error)
+    try {
+      if (window) {
+        // Lazy load this part if the client supports it
+        import('graphql-subscriptions-client').then(({ SubscriptionClient }) => {
+          this.subscriptionClient = new SubscriptionClient(wsUrl, {
+            reconnect: true,
+            lazy: true, // only connect when there is a query
+            connectionCallback: error => {
+              error && console.error(error)
+            }
+          });
+        })
       }
-    });
+    } catch (err) {
+      this.subscriptionClient = null
+    }
   }
 
-  query(args): AxiosPromise<any>  {
+  query(args, { headers } = { headers: undefined }): AxiosPromise<any> {
+    // Merge current headers (specific to this query) with global headers (provided by the constructor function)
+    const currentHeaders = {
+      ...this.headers,
+      ...headers
+    };
+
     if (Array.isArray(args)) {
       let finalQuery = '';
       for (let i = 0; i < args.length; i++) {
@@ -39,7 +53,7 @@ export default class GraphqlClient {
         if (i === 0) {
           finalQuery += print(args[i].query).slice(0, -3);
         } else {
-          finalQuery +=  print(args[i].query).slice(1);
+          finalQuery += print(args[i].query).slice(1);
         }
       }
 
@@ -47,9 +61,9 @@ export default class GraphqlClient {
         url: this.url,
         method: 'POST',
         data: { query: finalQuery },
-        headers: this.headers
+        headers: currentHeaders
       })
-      .then(({ data }) => data);
+        .then(({ data }) => data);
     }
 
     const { query, variables } = args;
@@ -63,13 +77,13 @@ export default class GraphqlClient {
       url: this.url,
       method: 'POST',
       data: { query: queryString, variables },
-      headers: this.headers
+      headers: currentHeaders
     })
       .then(({ data }) => data);
   }
 
-  mutation({ query, variables }): AxiosPromise<any>  {
-    return this.query({ query, variables });
+  mutation({ query, variables }, opts): AxiosPromise<any> {
+    return this.query({ query, variables }, opts);
   }
 
   subscribe({ query, variables }) {
