17ce1c3a2b3421c8e5484aef620544bc69c81af2
Tchoupinax <corentinfiloche@hotmail.fr>
2023-12-21 10:37:37 +0100

    validation with Nuxt.js v3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import GraphqlClient from './client';
-import VuePlugin from './vue-plugin';
+// Vue 2 : https://vuejs.org/v2/guide/plugins.html
+// Vue 3 : https://v3.vuejs.org/guide/plugins.html#writing-a-plugin
+
+import client from './client';
+
+export interface IOptions {
+  url: string
+  headers: any
+}
 
 export default {
-  GraphqlClient,
-  install: VuePlugin.install,
+  install: (app: any, options: IOptions) => {
+    app.config.globalProperties.$graphqlClient = new client(options.url, options.headers);
+    app.provide('$graphqlClient', options);
+  },
 };
+export const GraphqlClient = client;
